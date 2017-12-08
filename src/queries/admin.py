6e4597a6cae23bb889303cc11540e25e9048ab59
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-12-08 12:55:31 -0600

    Add models to admin

--- src/queries/admin.py
+++ src/queries/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Dataset)
+admin.site.register(Video)
+admin.site.register(Query)
+admin.site.register(MatchArray)
+admin.site.register(Signature)
