0b02a5b13a90180438209822729b5a057bc09bc7
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-12-19 14:13:18 -0600

    Enable Auth

--- src/api/settings.py
+++ src/api/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
