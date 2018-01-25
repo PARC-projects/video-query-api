0cf6023670b861a124301683fe1dbc57fd5e36a1
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-01-25 16:23:33 -0600

    Clean settings file

--- src/api/settings.py
+++ src/api/settings.py
@@ -57,9 +57,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-     'DEFAULT_PAGINATION_CLASS': 'queries.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'queries.pagination.CustomPagination',
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'PAGE_SIZE': 10
 }
