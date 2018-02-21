4b97b9c33b3a0ed92a3a935f200bddb108d1521d
fetorres <frank.home@sbcglobal.net>
2018-02-21 12:50:30 -0800

    modified dnn_stream.py, feature.py and video_clip.py to include UNIQUE constraints, error messages for those constraints, and help text (a form of documentation even if it is never used in the app).

--- src/queries/models/video_clip.py
+++ src/queries/models/video_clip.py
@@ -1,13 +1,20 @@
 from django.db import models
 from queries.models import Video
+from django.core.exceptions import NON_FIELD_ERRORS
 
 
 class VideoClip(models.Model):
     video = models.ForeignKey(Video, on_delete=models.PROTECT)
-    clip = models.PositiveIntegerField();
-    duration = models.PositiveIntegerField(default=10);
-    debug_video_uri = models.CharField(max_length=4096, null=True)
-    notes = models.TextField(null=True)
+    clip = models.PositiveIntegerField()
+    duration = models.PositiveIntegerField(default=10)
+    debug_video_uri = models.CharField(max_length=4096, null=True, blank=True)
+    notes = models.TextField(max_length=25600, blank=True, null=True)
 
     class Meta:
         db_table = 'video_clip'
+        unique_together = (('video', 'clip', 'clip_duration'),)
+        error_messages = {
+            NON_FIELD_ERRORS: {
+                'unique_together': "(video, clip, clip_duration) in row of video_clips table is not unique.",
+            }
+        }
