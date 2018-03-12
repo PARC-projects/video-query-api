20689e37d17e7f5e833ffe0fd22696bebdb42b78
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-12 12:03:00 -0500

    Add new and revised endpoints

--- src/queries/views/query.py
+++ src/queries/views/query.py
@@ -1,9 +1,10 @@
+from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from queries.serializers import QuerySerializer, QueryResultSerializer, MatchSerializer
-from queries.models import Query, QueryResult, Match
+from queries.serializers import QuerySerializer, QueryResultSerializer, MatchSerializer, VideoSerializer
+from queries.models import Query, QueryResult, Match, Video
 
 
 class QueryViewSet(viewsets.ModelViewSet):
@@ -21,8 +22,48 @@ class QueryViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['get'])
     def query_result(self, request, pk):
+        """
+        GET Latest QueryResult base on Query Id
+        :param request:
+        :param pk: Query Id
+        :return: QueryResult
+        """
         return Response(QueryResultSerializer(QueryResult.get_latestest_query_result_by_query_id(pk), many=False).data)
 
     @detail_route(methods=['get'])
     def matches(self, request, pk):
+        """
+        GET latest Match(s) based on Query Id
+        :param request:
+        :param pk: Query Id
+        :return: Match[]
+        """
         return Response(MatchSerializer(Match.get_latestest_matches_by_query_id(pk), many=True).data)
+
+    @detail_route(methods=['get'])
+    def new_matches(self, request, pk):
+        """
+        GET query state that represents a new query ready to get its similarities computed
+        Polled by broker in algorithm project
+        :param request:
+        :param pk:
+        :return:
+        """
+        query = QueryResultSerializer(QueryResult.get_latestest_query_result_by_query_id(pk), many=False).data
+        return JsonResponse(query)
+
+    @detail_route(methods=['get'])
+    def revised_matches(self, request, pk):
+        """
+        GET query state that represents a  query ready to get its similarities revised
+        Polled by broker in algorithm project
+        :param request:
+        :param pk: Query Id
+        :return:
+        """
+        query = QueryResultSerializer(QueryResult.get_latestest_query_result_by_query_id(pk), many=False).data
+        matches = MatchSerializer(Match.get_latestest_matches_by_query_id(pk), many=True).data
+        return JsonResponse({
+            "query": query,
+            "matches": matches
+        })
