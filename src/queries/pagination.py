49576a196269451005ac8512c29e2d81edf64105
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-04-03 12:47:59 -0500

    Unify naming style

--- src/queries/pagination.py
+++ src/queries/pagination.py
@@ -8,16 +8,16 @@ class CustomPagination(pagination.PageNumberPagination):
         if self.page.has_previous():
             previous_page = self.page.previous_page_number()
 
-        nextPage = None;
+        next_page = None;
         if self.page.has_next():
-            nextPage = self.page.next_page_number()
+            next_page = self.page.next_page_number()
 
         return Response({
             'pagination': {
                 'count': self.page.paginator.count,
                 'previousPage': previous_page,
                 'currentPage': self.page.number,
-                'nextPage': nextPage,
+                'nextPage': next_page,
                 'lastPage': self.page.paginator.num_pages
             },
             'results': data
