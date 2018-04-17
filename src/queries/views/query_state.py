0a024e60c9060b84ed919e15d80cfadc507e2940
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-04-17 16:38:23 -0500

    Add non content response

--- src/queries/views/query_state.py
+++ src/queries/views/query_state.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from queries.serializers import QuerySerializer, QueryResultSerializer, MatchSerializer, VideoSerializer
-from queries.models import Query, QueryResult, Match, Video
+from rest_framework.response import Response
 
+from queries.serializers import QuerySerializer, QueryResultSerializer, MatchSerializer
+from queries.models import Query, QueryResult, Match
+from rest_framework import status
 
 @api_view(['GET'])
 def compute_new_state(request):
@@ -25,13 +27,16 @@ def compute_revised_state(request):
     Polled by broker in algorithm project
     """
     query = QuerySerializer(Query.get_latest_query_ready_for_compute_similarity(), many=False).data
-    print(query)
-    results = QueryResultSerializer(QueryResult.get_latestest_query_result_by_query_id(query["id"]), many=False).data
-    matches = MatchSerializer(Match.get_latest_matches_by_query_id(query["id"]), many=True).data
-    return JsonResponse({
-        "query_id": query["id"],
-        "video_id": query["video"],
-        "reference_time": query["reference_time"],
-        "result": results,
-        "matches": matches
-    })
+    if 'id' in query:
+        results = QueryResultSerializer(QueryResult.get_latestest_query_result_by_query_id(query["id"]),
+                                        many=False).data
+        matches = MatchSerializer(Match.get_latest_matches_by_query_id(query["id"]), many=True).data
+        return JsonResponse({
+            "query_id": query["id"],
+            "video_id": query["video"],
+            "reference_time": query["reference_time"],
+            "result": results,
+            "matches": matches
+        })
+
+    return Response("No revised queries were found.", status=status.HTTP_204_NO_CONTENT);
