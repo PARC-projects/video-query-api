ee0fdbf27c7858e52ed8315841ed1cb6908d9311
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-04-17 16:53:04 -0500

    Add no content check to new state GET

--- src/queries/views/query_state.py
+++ src/queries/views/query_state.py
@@ -6,6 +6,7 @@ from queries.serializers import QuerySerializer, QueryResultSerializer, MatchSer
 from queries.models import Query, QueryResult, Match
 from rest_framework import status
 
+
 @api_view(['GET'])
 def compute_new_state(request):
     """
@@ -13,11 +14,14 @@ def compute_new_state(request):
     Polled by broker in algorithm project
     """
     query = QuerySerializer(Query.get_latest_query_ready_for_new_compute_similarity(), many=False).data
-    return JsonResponse({
-        "query_id": query["id"],
-        "video_id": query["video"],
-        "reference_time": query["reference_time"]
-    })
+    if 'id' in query:
+        return JsonResponse({
+            "query_id": query["id"],
+            "video_id": query["video"],
+            "reference_time": query["reference_time"]
+        })
+    
+    return Response("No new queries were found.", status=status.HTTP_204_NO_CONTENT);
 
 
 @api_view(['GET'])
