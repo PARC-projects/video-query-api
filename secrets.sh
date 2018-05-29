bcc300b9639b1106cf690b3281e2356701bfbf00
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-29 12:32:57 -0500

    fixed untracked files - secrets.sh

--- secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# on Mac, execute 'source secrets.sh' from command line
-export API_DEBUG='False'
-export API_SECRET_KEY='983471509835445'
-export API_DB_NAME='Video-Query'
-export API_DB_USER='torres'
-export API_DB_PASS='fetorres'
-export API_DB_HOST='localhost'
-export API_DB_PORT='5432'
