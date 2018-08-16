832e53d59070fdeccaabf27d04d01790ccf52e93
fetorres <frank.home@sbcglobal.net>
2018-08-15 18:48:38 -0700

    add final_report model and view and serializer

--- src/queries/models/final_report.py
+++ src/queries/models/final_report.py
@@ -3,9 +3,12 @@ from . import Query
 
 
 class FinalReport(models.Model):
-    query = models.ForeignKey(Query, on_delete=models.PROTECT, unique=True, help_text='Query id')
+    query = models.OneToOneField(Query, on_delete=models.PROTECT, primary_key=True, help_text='Query id')
     file_name = models.CharField(max_length=254, unique=True, help_text='file name for final csv report')
     last_modified = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
 
     class Meta:
         db_table = 'final_reports'
+
+    def __str__(self):
+        return "Final report for query %s" % self.query.name
