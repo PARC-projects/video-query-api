a93132ffe28929fea67242bbba8232901d65ce9c
fetorres <frank.home@sbcglobal.net>
2018-08-17 10:55:56 -0700

    generalizing the MEDIA-ROOT name, for future flexibility.

--- src/api/settings.py
+++ src/api/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 MEDIA_URL = '/files/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, 'algorithm_reports')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'file repository')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
