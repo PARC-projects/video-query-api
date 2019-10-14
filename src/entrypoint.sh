1b3be3128ecc523c2d4caa1d743791a4a6556a7f
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-10-14 10:11:57 -0500

    Implement gunicorn

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -11,11 +11,16 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+# https://docs.djangoproject.com/en/2.2/ref/django-admin/#flush
+# Removes all data from the database and re-executes any post-synchronization handlers.
+# The table of which migrations have been applied is not cleared.
+# python manage.py flush --no-input
+
+# Migations
 python manage.py migrate
 python manage.py loaddata 1-lookups
 
-# Development
+# Development Migrations
 # python3 manage.py loaddata 2-dev-video 3-dev-search-set 4-dev-query 5-dev-results 6-dev-features 7-dev-video-clip
 
 exec "$@"
