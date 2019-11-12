b00fc8317bda29310a2eb6c8031b221c6bca3255
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-11-12 17:52:21 -0600

    Update vs-code settings

--- src/queries/models/query.py
+++ src/queries/models/query.py
@@ -7,24 +7,26 @@ from . import SearchSet, Video, ProcessState, VideoClip
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return 'final_reports/user_{0}/{1}'.format(instance.user.id, filename)
-    
+
     and change to upload_to=user_directory_path
 '''
 
-
 class Query(models.Model):
     name = models.CharField(max_length=254, unique=True)
-    search_set_to_query = models.ForeignKey(SearchSet, on_delete=models.PROTECT)
+    search_set_to_query = models.ForeignKey(
+        SearchSet, on_delete=models.PROTECT)
     video = models.ForeignKey(Video, on_delete=models.PROTECT)
     reference_time = models.DurationField(default='00:00:00')
     max_matches_for_review = models.PositiveIntegerField(default=20)
     notes = models.TextField(null=True)
     reference_clip_image = models.ImageField(null=True)
-    process_state = models.ForeignKey(ProcessState, default=1, on_delete=models.PROTECT)
+    process_state = models.ForeignKey(
+        ProcessState, default=1, on_delete=models.PROTECT)
     final_report_file = models.FileField(max_length=254, upload_to='final_reports/', null=True,
                                          help_text='final csv report. For coreapi, use encoding in client.action(): '
                                                    'client.action(..., encoding="multipart/form-data")')
-    last_modified = DateTimeField(auto_now=True, editable=False, null=False, blank=False)
+    last_modified = DateTimeField(
+        auto_now=True, editable=False, null=False, blank=False)
     # Let the algorithms know whether to “average” the features of all validated matches in each round and have that be
     # the new reference.
     use_dynamic_target_adjustment = models.BooleanField(default=False)
